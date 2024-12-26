2c84e852f2a041f0a29e9a5e116dae1ffbbd4922
Nachiketha <nachiketh.k.c@brainsightai.com>
2024-12-26 11:11:29 +0530

    interface branch

---
+++ OOPS/interface.ts
@@ -0,0 +1,47 @@
+interface User{
+    name:String,
+    email:String,
+    password:String,
+    age:Number,
+    address?:String,
+    // display:()=> void
+    display():void
+}
+
+let user:User = {
+    name:'John',
+    email:'john@example.com',
+    password:'john@123',
+    age:25,
+    display:function(){
+        console.log('Name:',this.name);
+        console.log('Email:',this.email);
+        console.log('Password:',this.password);
+
+    }
+}
+
+user.display();
+
+
+interface Admin extends User{
+    role: string;
+}
+
+let admin:Admin = {
+    name:'Admin',
+    email:'admin@gmail.com',
+    password:'admin@123',
+    age:30,
+    role:'Admin',
+    display:function(){
+        console.log('Name:',this.name);
+        console.log('Email:',this.email);
+        console.log('Password',this.password);
+        console.log('Role:',this.role);
+    }
+}
+
+admin.display();
+
+export {};
